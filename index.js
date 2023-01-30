e7887b38957ca9b5fc5b26148f6fe37fac511c82
zinakalu <zina.kalu@gmail.com>
2023-01-30 19:19:12 +0100

    created js events listening lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
